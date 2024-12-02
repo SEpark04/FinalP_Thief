2a4ee20c25ca38f2b5b06ff78e9a584256465d4e
chaeyun-back <backcy02@naver.com>
2024-12-02 19:19:40 +0900

    12.02_19:20
    -Map 씬 장애물 코드 수정 중

--- Assets/BCY/Map_Test_Scripts/Obstacle_turn_2_Ctrl.cs
+++ Assets/BCY/Map_Test_Scripts/Obstacle_turn_2_Ctrl.cs
@@ -4,14 +4,55 @@ using UnityEngine;
 
 public class Obstacle_turn_2_Ctrl : MonoBehaviour
 {
+    public GameObject target;
+    public float fieldOfViewAngle = 50f;
+    public float viewDistance = 10f;
+    private bool isLockedOn = false;
+
+    //회전 변수
     public float rotationSpeed = 10f;
     public float moveSpeed = 5f;
-
-
-    
+   
+   
     void Update()
     {
-        transform.Rotate(rotationSpeed, 0, 0);
-        transform.position -= new Vector3(0, 0, moveSpeed * Time.deltaTime);
+        CheckForTarget();
+
+
+        if (isLockedOn)
+        {
+            transform.Rotate(rotationSpeed, 0, 0);
+            transform.position -= new Vector3(0, 0, moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            enabled = false;
+        }
+
+
+    }
+
+    void CheckForTarget()
+    {
+        Vector3 directionToTarget = target.transform.position - transform.position;
+        float angleToTarget = Vector3.Angle(transform.forward, directionToTarget);
+
+        if (angleToTarget <= fieldOfViewAngle / 2 && directionToTarget.magnitude <= viewDistance)
+        {
+            Ray ray = new Ray(transform.position, directionToTarget);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, viewDistance))
+            {
+                if (hit.transform == target.transform)
+                {
+                    isLockedOn = true; // 타겟이 시야 내에 있음
+                    return;
+                }
+            }
+
+        }
+
+        isLockedOn = false;
     }
 }
